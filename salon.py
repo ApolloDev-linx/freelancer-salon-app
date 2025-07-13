2c44ffcb5b55f46309666cb8d39f0efea1501e49
ApolloDev-linx <bossangel1127@gmail.com>
2025-07-13 15:12:39 -0500

    Add Flask backend structure

--- salon.py
+++ salon.py
@@ -1,1 +1,12 @@
-print("backend")
+from flask import Flask, request, jsonify,render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
