6790fedd602168c4638d9fd0ddd278e122a4e5c5
ApolloDev-linx <bossangel1127@gmail.com>
2025-07-14 19:07:43 -0500

    Set up flask POST route for salon data input and connected frontend fetch logic

--- salon.py
+++ salon.py
@@ -5,8 +5,32 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/home')
-def home_page():
+def home():
     return render_template('home.html')
+
+
+@app.route('/home', methods=["POST"])
+def home_page():
+    data = request.get_json()
+    worker = data["worker"]
+    service = data["service"]
+    amount = data["amount"]
+
+    return jsonify({
+        "worker": worker,
+        "service": service,
+        "amount": amount
+
+        })
+    
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/home.js
+++ static/home.js
@@ -1,1 +1,24 @@
-console.log("hello")
+document.getElementById("submit").addEventListener("click", ()=>{
+let worker = document.getElementById("worker").value;
+let service = document.getElementById("service").value;
+let amount = document.getElementById("amount").value;
+
+let payload = {
+	worker:worker,
+	service:service,
+	amount:amount
+		};
+fetch('/home',{
+	method: "POST",
+	headers:{"Content-type": "application/json"},
+	body: JSON.stringify(payload)
+		})
+.then(res => res.json())
+.then(data =>{
+console.log("test1",data);
+})
+.catch(error => {
+console.error("Problems sending data:", error);
+})
+});
+
